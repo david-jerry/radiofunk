a29a119556a25a362fd491ba69bbc2ab3b57ed17
Jeremiah David <jeremiahedavid@gmail.com>
2022-02-09 07:35:01 +0100

    fixed procfile

--- config/settings/production.py
+++ config/settings/production.py
@@ -107,7 +107,7 @@ EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com:587")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default='mail@gmail.com')# sendgrid
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="Paper123.")
 EMAIL_PORT = env("EMAIL_HOST_PORT", default="465")
-
+EMAIL_USE_SSL = True
 
 # LOGGING
 # ------------------------------------------------------------------------------
