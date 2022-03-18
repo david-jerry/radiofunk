c9a08c39b4d3f2ef225d2345d483f788ac8ac326
Jeremiah David <jeremiahedavid@gmail.com>
2022-03-18 07:19:48 -0400

    fixed active mode in genre

--- radio_funk/genre/signals.py
+++ radio_funk/genre/signals.py
@@ -6,8 +6,7 @@ from radio_funk.utils.logger import LOGGER
 
 from .models import Genre
 
-@receiver(post_save, sender=Genre)
-def bg_color_creator_signal(sender, created, instance, *args, **kwargs):
-    if created:
-        if instance.color is None:
-            instance.color = "#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+@receiver(pre_save, sender=Genre)
+def bg_color_creator_signal(sender, instance, *args, **kwargs):
+    if instance.color is None:
+        instance.color = "#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])
