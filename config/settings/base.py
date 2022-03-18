16d5aa238ee38329b46df9a32e0cc126a0637015
Jeremiah David <jeremiahedavid@gmail.com>
2022-03-18 01:42:22 -0400

    fixed migrations

--- config/settings/base.py
+++ config/settings/base.py
@@ -324,7 +324,7 @@ CSRF_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
-X_FRAME_OPTIONS = "SAMESITE"
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 # EMAIL
 # ------------------------------------------------------------------------------
