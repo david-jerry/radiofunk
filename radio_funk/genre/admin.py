129b3a4acb3dd588c2189e3e0427dccf45f0c29e
Jeremiah David <jeremiahedavid@gmail.com>
2022-03-18 07:13:34 -0400

    fixed active mode in genre

--- radio_funk/genre/admin.py
+++ radio_funk/genre/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Genre
 
-@admin.register(User)
+@admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ["name", "active", "color"]
 
